968f16bf582f984c23c91cbc524ce02cd72a9fe5
DIONSTAM <58949888+kuedonn@users.noreply.github.com>
2022-06-09 11:34:48 +0300

    added some stuff
    still using some code from the guide to learn the API, weatherjs is work in progress

---
+++ commands/weather.js
@@ -0,0 +1,32 @@
+//work in progress
+
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const  axios  = require('axios');
+
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('weather')
+        .setDescription('Shows weather in a specified location')
+        .addStringOption(option =>{
+            return option,
+            option.setName('input')
+            .setDescription('user input')
+            .setRequired(true)
+        }),
+        async execute(interaction){
+            const input = interaction.options.getString('input');
+            //cannot read properties of undefined reading 'get' sto axios
+            await axios.get("https://api.openweathermap.org/data/2.5/forecast?q="+input+"&appid="+process.env.WEATHER_APPID+"&units=metric").then(response =>{
+                console.log(response.data);
+            }).catch(err =>{(console.log(err))
+            });
+        },
+};
--- index.js
+++ index.js
@@ -22,9 +22,19 @@ for (const file of commandFiles){
 	client.commands.set(command.data.name, command);
 }
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+const eventsPath = path.join(__dirname,'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file=>file.endsWith('js'));
+
+for (const file of eventFiles){
+	const filePath = path.join(eventsPath,file);
+	const event = require(filePath);
+	if (event.once){
+		client.once(event.name,(...args)=> event.execute(...args));
+	} else {
+		client.on(event.name,(...args)=> event.execute(...args));
+	}
+}
+
 
 client.on('interactionCreate', async interaction =>{
 	if (!interaction.isCommand()) return;
