aeb8f0a1e4875b6147b0f92f99d3101cf757c770
DIONSTAM <58949888+kuedonn@users.noreply.github.com>
2022-06-09 13:18:20 +0300

    Update weather.js
    weather command works normally using /weather and user input of a city name

--- commands/weather.js
+++ commands/weather.js
@@ -1,6 +1,3 @@
-//work in progress
-
-
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const  axios  = require('axios');
@@ -8,7 +5,6 @@ const  axios  = require('axios');
 
 const dotenv = require('dotenv');
 dotenv.config();
-
 
 
 module.exports = {
@@ -23,10 +19,30 @@ module.exports = {
         }),
         async execute(interaction){
             const input = interaction.options.getString('input');
-            //cannot read properties of undefined reading 'get' sto axios
-            await axios.get("https://api.openweathermap.org/data/2.5/forecast?q="+input+"&appid="+process.env.WEATHER_APPID+"&units=metric").then(response =>{
-                console.log(response.data);
+            await axios.get("https://api.openweathermap.org/data/2.5/weather?q="+input+"&appid="+process.env.WEATHER_APPID+"&units=metric").then(response =>{  
+                const data = response.data;
+                console.log(data);
+
+                const embed = new MessageEmbed()
+                .setColor('#00FF99')
+                .setAuthor({name: 'Kuedon#4714', url:'https://github.com/kuedonn'})
+                .setTitle('Current Weather')
+                .addFields(
+                {name: 'Location', value: data.name},
+                {name: 'Current',value: data.weather[0].description},
+                {name: 'Temperature', value: (data.main.temp).toString()+'C'},
+                {name: 'Wind', value: (data.wind.speed).toString()+'m/s'},
+                {name: 'Humidity', value: (data.main.humidity).toString()+'%'})
+                .setFooter({text: 'Created by Kuedon 2022'});
+                interaction.reply({embeds: [embed] });
+                
+                
             }).catch(err =>{(console.log(err))
             });
+            
+
+            
+                
         },
 };
+
